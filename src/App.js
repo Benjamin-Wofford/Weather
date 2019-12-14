25aa700e9a9943c075cbdb3ac57e9922104ce555
Critter4Dinner <woffordben1992@gmail.com>
2019-12-14 15:53:17 -0500

    Designed the favicon for the project and updated it

--- src/App.js
+++ src/App.js
@@ -21,8 +21,9 @@ class App extends React.Component{
     e.preventDefault()
     const city = e.target.elements.city.value
     const country = e.target.elements.country.value
-    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/find?q=${city},${country}&appid=${API_KEY}`)
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/find?q=${city},${country}&appid=${API_KEY}&units=imperial`)
     const data = await api_call.json();
+    if (city && country){
     console.log(data)
     this.setState({
       temperature: data.list[0].main.temp,
@@ -30,8 +31,19 @@ class App extends React.Component{
       country: data.list[0].sys.country,
       humidity: data.list[0].main.humidity,
       description: data.list[0].weather[0].description,
+       }
+    )
+  } else {
+    this.setState({
+      temperature: undefined,
+      city: undefined,
+      country: undefined,
+      humidity: undefined, 
+      description: undefined,
+      error: "Please enter a city and country."
     })
   }
+}
 
  render(){
 
@@ -45,6 +57,7 @@ class App extends React.Component{
       country={this.state.country}
       humidity={this.state.humidity}
       description={this.state.description}
+      error={this.state.error}
     />
   </>
   )
