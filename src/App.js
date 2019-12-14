717e7dd322f31a80cea3ed26fad2dad2abd04221
Critter4Dinner <woffordben1992@gmail.com>
2019-12-14 09:11:58 -0500

    layed out form structure for Form component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import React from 'react';
 import Titles from './components/Titles'
+import Form from './components/Form'
+import Weather from './components/Weather'
 import './App.css';
 
-function App() {
-  return (<Titles/>)
-}
+const API_KEY = 'd37451dc514e7fb035ba3d90e7d46b50'
+
+class App extends React.Component{
+
+  getWeather = async () => {
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/find?q=London&appid=${API_KEY}`)
+    const data = await api_call.json();
+    console.log(data)
+  }
+
+ render(){
+ return (
+  <>
+   <Titles/>
+    <Form/>
+    <Weather/>
+  </>
+  )
+}}
+
 
 export default App;
