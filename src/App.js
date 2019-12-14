11a68d72c62bb9d5acb07ddaac2ac460df1777ed
Critter4Dinner <woffordben1992@gmail.com>
2019-12-14 09:51:45 -0500

    Added state to App component

--- src/App.js
+++ src/App.js
@@ -7,9 +7,20 @@ import './App.css';
 const API_KEY = 'd37451dc514e7fb035ba3d90e7d46b50'
 
 class App extends React.Component{
-
-  getWeather = async () => {
-    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/find?q=London&appid=${API_KEY}`)
+  state = {
+    temperature: undefined,
+    city: undefined, 
+    country: undefined, 
+    humidity: undefined, 
+    description: undefined, 
+    error: undefined
+  }
+  
+  getWeather = async (e) => {
+    e.preventDefault()
+    const city = e.target.elements.city.value
+    const country = e.target.elements.country.value
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/find?q=${city},${country}&appid=${API_KEY}`)
     const data = await api_call.json();
     console.log(data)
   }
@@ -18,7 +29,7 @@ class App extends React.Component{
  return (
   <>
    <Titles/>
-    <Form/>
+    <Form getWeather={this.getWeather}/>
     <Weather/>
   </>
   )
