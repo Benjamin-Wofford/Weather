a7761d7d680d8ecf71a0762a829038259f0b8af9
Critter4Dinner <woffordben1992@gmail.com>
2019-12-14 10:50:23 -0500

    displaying weather data consumed from API call

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import './App.css';
 const API_KEY = 'd37451dc514e7fb035ba3d90e7d46b50'
 
 class App extends React.Component{
+
   state = {
     temperature: undefined,
     city: undefined, 
@@ -15,7 +16,7 @@ class App extends React.Component{
     description: undefined, 
     error: undefined
   }
-  
+
   getWeather = async (e) => {
     e.preventDefault()
     const city = e.target.elements.city.value
@@ -23,14 +24,28 @@ class App extends React.Component{
     const api_call = await fetch(`http://api.openweathermap.org/data/2.5/find?q=${city},${country}&appid=${API_KEY}`)
     const data = await api_call.json();
     console.log(data)
+    this.setState({
+      temperature: data.list[0].main.temp,
+      city: data.list[0].name,
+      country: data.list[0].sys.country,
+      humidity: data.list[0].main.humidity,
+      description: data.list[0].weather[0].description,
+    })
   }
 
  render(){
+
  return (
   <>
    <Titles/>
     <Form getWeather={this.getWeather}/>
-    <Weather/>
+    <Weather 
+      temperature={this.state.temperature}
+      city={this.state.city}
+      country={this.state.country}
+      humidity={this.state.humidity}
+      description={this.state.description}
+    />
   </>
   )
 }}
