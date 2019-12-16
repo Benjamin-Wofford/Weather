3969a0db59af516c4808534c38e3f37e9700cd6a
Critter4Dinner <woffordben1992@gmail.com>
2019-12-15 23:28:04 -0500

    project deployed to firebase at m

--- src/App.js
+++ src/App.js
@@ -21,10 +21,9 @@ class App extends React.Component{
     e.preventDefault()
     const city = e.target.elements.city.value
     const country = e.target.elements.country.value
-    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/find?q=${city},${country}&appid=${API_KEY}&units=imperial`)
+    const api_call = await fetch(`https://api.openweathermap.org/data/2.5/find?q=${city},${country}&appid=${API_KEY}&units=imperial`)
     const data = await api_call.json();
     if (city && country && data.count !== 0){
-    console.log(data)
     this.setState({
       temperature: data.list[0].main.temp,
       city: data.list[0].name,
