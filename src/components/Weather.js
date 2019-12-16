66c2d479bd0714685bb6cf415321c3b1e0d4b376
Critter4Dinner <woffordben1992@gmail.com>
2019-12-15 20:19:53 -0500

    Further styled the right side of the main window.

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -4,9 +4,9 @@ class Weather extends React.Component{
     render(){
         return(
             <>
-                {this.props.city && this.props.country && <p><span>Location: </span> {this.props.city},{this.props.country}</p>}
-                {this.props.temperature && <p><span>Temperature: </span>{this.props.temperature}</p>}
-                {this.props.humidity && <p><span>Humidity: </span> {this.props.humidity}</p>}
+                {this.props.city && this.props.country && <><p><span>Location: </span> {this.props.city},{this.props.country}</p><hr/></>}
+                {this.props.temperature && <><p><span>Temperature: </span>{this.props.temperature} F</p><hr/></>}
+                {this.props.humidity && <><p><span>Humidity: </span> {this.props.humidity}%</p><hr/></>}
                 {this.props.description && <p><span>Conditions: </span> {this.props.description}</p>}
                 {this.props.error && <p><span>Error: </span>{this.props.error}</p>}
             </>
